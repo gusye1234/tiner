f0df68d94d82680aa671ab4a44e177ca52fd9e6d
Jianbai Ye <jianbaiye@outlook.com>
2022-09-30 14:16:08 +0800

    docs: update url
    Signed-off-by: Jianbai Ye <jianbaiye@outlook.com>

---
+++ README.md
@@ -0,0 +1,4 @@
+<div align="center">
+  <h1>tiner</h1>
+  <p><strong>Block-wise timer for Python</strong></p>
+</div>
--- tiner/__init__.py
+++ tiner/__init__.py
@@ -3,4 +3,4 @@ from .tiner import tiner
 
 __author__ = "Gustavo Ye"
 __version__ = "0.0.1"
-__url__ = 'PLACEHOLDER'
+__url__ = 'https://github.com/gusye1234/tiner'
