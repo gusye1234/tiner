ec8da1e6d43aaa1d59bdac3c723756b328e5ca2e
Jianbai Ye <jianbaiye@outlook.com>
2022-10-05 15:43:10 +0800

    fix: setup deps error
    Signed-off-by: Jianbai Ye <jianbaiye@outlook.com>

--- setup.py
+++ setup.py
@@ -1,15 +1,24 @@
 import setuptools
-import tiner
 
 with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 
+vars2find = ['__author__', '__version__', '__url__']
+varstiner = {}
+with open("./tiner/__init__.py") as f:
+    for line in f.readlines():
+        for v in vars2find:
+            if line.startswith(v):
+                line = line.replace(" ", '').replace(
+                    "\"", '').replace("\'", '').strip()
+                varstiner[v] = line.split('=')[1]
+
 setuptools.setup(
     name='tiner',
-    url=tiner.__url__,
-    version=tiner.__version__,
-    author=tiner.__author__,
+    url=varstiner['__url__'],
+    version=varstiner['__version__'],
+    author=varstiner['__author__'],
     description='Block-wise timer for Python',
     long_description=long_description,
     long_description_content_type='text/markdown',
--- tiner/__init__.py
+++ tiner/__init__.py
@@ -2,5 +2,5 @@ from .tiner import tiner
 
 
 __author__ = "Gustavo Ye"
-__version__ = "0.0.6"
+__version__ = "0.0.7"
 __url__ = 'https://github.com/gusye1234/tiner'
